45e6035cc5ae930f64db28c5759b699d002d17ee
shamiruf <rufashamikh@gmail.com>
2023-11-18 19:46:01 +0300

    createOrder, getActiveOrders

--- src/create-order.js
+++ src/create-order.js
@@ -1,41 +1,50 @@
-import { AuctionSalt, AuctionSuffix, FusionOrder } from '@1inch/fusion-sdk'
+import { AuctionSalt, AuctionSuffix, FusionOrder } from "@1inch/fusion-sdk";
 
+// TODO!
 const salt = new AuctionSalt({
   duration: 180,
   auctionStartTime: 1673548149,
   initialRateBump: 50000,
-  bankFee: '0'
-})
+  bankFee: "0",
+});
 
+// TODO!
 const suffix = new AuctionSuffix({
   points: [
     {
       coefficient: 20000,
-      delay: 12
-    }
+      delay: 12,
+    },
   ],
   whitelist: [
     {
-      address: '0x00000000219ab540356cbb839cbe05303d7705fa',
-      allowance: 0
-    }
-  ]
-})
+      address: "0x00000000219ab540356cbb839cbe05303d7705fa",
+      allowance: 0,
+    },
+  ],
+});
 
-const order = new FusionOrder(
-  {
-    makerAsset: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-    takerAsset: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    makingAmount: '1000000000000000000',
-    takingAmount: '1420000000',
-    maker: '0x00000000219ab540356cbb839cbe05303d7705fa'
-  },
-  salt,
-  suffix
-)
+export const createFusionOrder = (
+  makerAsset,
+  takerAsset,
+  makingAmount,
+  takingAmount,
+  maker
+) => {
+  const order = new FusionOrder(
+    {
+      makerAsset,
+      takerAsset,
+      makingAmount,
+      takingAmount,
+      maker,
+    },
+    salt,
+    suffix
+  );
 
-order.build()
-/* #=> {
+  const fusionOrder = order.build();
+  /* #=> {
             allowedSender: '0x0000000000000000000000000000000000000000',
             interactions:
                 '0x000c004e200000000000000000219ab540356cbb839cbe05303d7705faf486570009',
@@ -49,20 +58,5 @@ order.build()
             takingAmount: '1420000000'
         }
 */
-
-export const createFusionOrder = () => {
-  const order = new FusionOrder(
-    {
-      makerAsset: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-      takerAsset: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-      makingAmount: '1000000000000000000',
-      takingAmount: '1420000000',
-      maker: '0x00000000219ab540356cbb839cbe05303d7705fa'
-    },
-    salt,
-    suffix
-  )
-  
-  const fusionOrder = order.build()
-  return fusionOrder
-}
+  return fusionOrder;
+};
--- src/index.js
+++ src/index.js
@@ -1,61 +1,91 @@
-import express from 'express';
-import { Waku, WakuMessage } from 'js-waku';
-import bodyParser from 'body-parser';
-import { createFusionOrder } from './create-order.js';
+import express from "express";
+import { Waku, WakuMessage } from "js-waku";
+import bodyParser from "body-parser";
+import { createFusionOrder } from "./create-order.js";
+import { syncOrderbookWithFusion } from "./get-active-orders.js";
 
 const app = express();
 const port = 3000;
 
 // Initialize an array to store the orders
-let orderBook  = [];
+let orderBook = [];
 
 app.use(bodyParser.json());
 
 async function startWaku() {
-	const waku = await Waku.create({
-		bootstrap: {
-			default: true,
-		},
-	});
+  const waku = await Waku.create({
+    bootstrap: {
+      default: true,
+    },
+  });
 
-	console.log('Waku node created and started.');
+  console.log("Waku node created and started.");
 
-	// Listen for messages on the /orderbook topic
-	waku.relay.addObserver((msg) => {
-		if (msg.contentTopic === '/orderbook') {
-			const messageText = new TextDecoder().decode(msg.payload);
-			try {
-				const order = JSON.parse(messageText);
-				orderBook.push(order);
-				console.log('Order added to orderBook:', order);
-			} catch (error) {
-				console.error('Error parsing message:', error);
-			}
-		}
-	}, ['/orderbook']);
+  // Listen for messages on the /orderbook topic
+  waku.relay.addObserver(
+    (msg) => {
+      if (msg.contentTopic === "/orderbook") {
+        const messageText = new TextDecoder().decode(msg.payload);
+        try {
+          const order = JSON.parse(messageText);
+          orderBook.push(order);
+          console.log("Order added to orderBook:", order);
+        } catch (error) {
+          console.error("Error parsing message:", error);
+        }
+      }
+    },
+    ["/orderbook"]
+  );
 
-	// Handle POST request to /create-order
-	app.post('/create-order', async (req, res) => {
-		// const order = req.body;
-		const order = await createFusionOrder()
-		console.log('Received order:', order);
+  // Handle POST request to /create-order
+  app.post("/create-order", async (req, res) => {
+    console.log("Handle POST request to /create-order");
 
-		const payload = new TextEncoder().encode(JSON.stringify(order));
-		await waku.relay.send(await WakuMessage.fromBytes(payload, '/orderbook'));
+    try {
+      const orderInfo = req.body;
+      console.log("Request body: ", orderInfo);
 
-		res.send('Order added to the orderbook');
-	});
+      const order = await createFusionOrder(orderInfo);
+      console.log("Received order:", order);
 
-	// Handle GET request to /orderbook
-	app.get('/orderbook', (req, res) => {
-		res.json(orderBook);
-	});
+      const payload = new TextEncoder().encode(JSON.stringify(order));
+      await waku.relay.send(await WakuMessage.fromBytes(payload, "/orderbook"));
 
-	app.listen(port, () => {
-		console.log(`Server listening at http://localhost:${port}`);
-	});
+      // Push new order to orderBook
+      orderBook.push(order);
+
+      res.send("Order added to the orderbook");
+    } catch (e) {
+      console.error(e);
+      console.error("ERROR MESSAGE:  ", e.messageText);
+    }
+  });
+
+  // Handle GET request to /orderbook
+  app.get("/orderbook", async (req, res) => {
+    try {
+      const { page, limit } = req.body;
+      console.log("Request body /orderbook: ", req.body);
+
+      const allActiveOrder = await syncOrderbookWithFusion(
+        page,
+        limit,
+        orderBook
+      );
+
+      res.json(allActiveOrder);
+    } catch (e) {
+      console.error("ERROR GET /orderbook");
+      console.error(e);
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
 }
 
 startWaku().catch((e) => {
-	console.error('Error starting Waku node:', e);
+  console.error("Error starting Waku node:", e);
 });
